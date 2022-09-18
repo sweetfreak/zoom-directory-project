26e367926df4d9434ccfa6b5da0df9d31e8ef5eb
Muhammad Humayun <humayun@MacBook-Pro.home>
2022-09-18 13:41:26 -0400

    added comments and made the code look neat

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -10,14 +10,14 @@ module.exports = {
     // allows token to be sent via req.body, req.query, or headers
     let token = req.body.token || req.query.token || req.headers.authorization;
 
-  
+    //checking for the authorization so the following methods be executed
     if (req.headers.authorization) {
       token = token
         .split(' ')
         .pop()
         .trim();
     }
-
+    //otherwise it would return the request 
     if (!token) {
       return req;
     }
